3b4a496143a45b6da8a01844f8a99305072ba127
Bert Kleewein <bertk@microsoft.com>
2016-09-29 13:28:42 -0700

    also use ping

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 
 'use strict';
 
-function isHostUp(ip, done, provider) {
+function isHostUpNmap(ip, done, provider) {
   if (!ip) throw new Error('ip is required');
   if (!done) throw new Error('done is required');
 
   var spawn = provider || require('child_process').spawn;
-  var nmap = spawn('nmap', ['-Pn', ip]);
+  var nmap = spawn('nmap', [ip]);
 
   nmap.stdout.on('data', function(data) {
    if (data.toString('ascii').indexOf('(1 host up)') > -1) {
@@ -35,7 +35,44 @@ function isHostUp(ip, done, provider) {
 
 }
 
+function isHostUpPing(ip, done, provider) {
+  if (!ip) throw new Error('ip is required');
+  if (!done) throw new Error('done is required');
+
+  var spawn = provider || require('child_process').spawn;
+  var nmap = spawn('ping', ['-c', '1', ip]);
+
+  nmap.stdout.on('data', function(data) {
+   if (data.toString('ascii').indexOf('1 received') > -1) {
+    if (done) {
+      done(null,true);
+      done = null;
+    }
+   }
+  });
+
+  nmap.on('exit', function() {
+    if (done) {
+      done(null,false);
+      done = null;
+    }
+  });
+
+  nmap.on('error', function() {
+    if (done) {
+      done(new Error('spawn error'));
+      done = null;
+    }
+  });
+
+}
+
+function isHostUp(ip, done) {
+  isHostUpPing(ip, function(err, isUp) {
+    if (err) return  done(err);
+    if (isUp) return done(null, true);
+    isHostUpNmap(ip, done);
+  });
+}
+
 module.exports = isHostUp;
-
-
-
